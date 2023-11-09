580fdbabb3b86cd769a62d8a9e02ff3d7611cc23
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2023-11-09 11:17:30 -0500

    Inserción de tablas dinámicas

--- secciones/agenda.php
+++ secciones/agenda.php
@@ -1,6 +1,6 @@
 <?php include('../plantillas/header.php'); ?>
 
-    Agenda de pacientes
+    <h1>Agenda de pacientes</h1>
 
     <div class="col-md-5">
     <div class="mb-3">
@@ -12,17 +12,30 @@
         aria-describedby="helpId"
         placeholder="ID">
     </div>
-    <div class="mb-3">
-      <label for="nombre_paciente" class="form-label">Nombre de paciente</label>
-      <input type="text"
-        class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
-      <small id="helpId" class="form-text text-muted">Help text</small>
-    </div>
+       <div class="mb-3">
+         <label for="nombre_paciente" class="form-label">Nombre de paciente</label>
+           <input type="text"
+             class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+           <small id="helpId" class="form-text text-muted">Ingrese nombre de paciente</small>
+       </div>
 
     </div>
-    <div class="col-md-7">
-
-        
+    <div class="container">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+         <h3 class="text-center">Listado de citas diarias</h3>
+         <div class="table-responsive table-hover" id="tablaConsulta">
+            <table class="table">
+              <thead class="text-muted">
+                  <th class="text-center">Número de identificación</th>
+                  <th class="text-center">Tipo</th>
+                  <th class="text-center">Nombres</th>
+                  <th class="text-center">Apellidos</th>
+                  <th class="text-center">Fecha y hora de cita</th>
+                  <th class="text-center">Estado de ingreso</th>
+              </thead>
+            </table></
+         </div>
+      </div>
     </div>
     
     
--- secciones/panel_pacientes.php
+++ secciones/panel_pacientes.php
@@ -1,5 +1,51 @@
+<?php
+include '../configuraciones/conecta.php';
+
+?>
+
 <?php include('../plantillas/header.php'); ?>
 
-    Control de pacientes en el sistema visto por recepcionista y médico
+<h1>Panel de pacientes</h1>
+
+<div class="col-md-5">
+<div class="mb-3">
+  <label for="" class="form-label">Ingrese ID de paciente</label>
+  <input type="text"
+    class="form-control"
+    name="id"
+    id="id"
+    aria-describedby="helpId"
+    placeholder="ID">
+</div>
+   <div class="mb-3">
+     <label for="nombre_paciente" class="form-label">Nombre de paciente</label>
+       <input type="text"
+         class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+       <small id="helpId" class="form-text text-muted">Ingrese nombre de paciente</small>
+   </div>
+
+</div>
+<div class="container">
+  <div class="col-sm-12 col-md-12 col-lg-12">
+     <h3 class="text-center">Listado de citas diarias</h3>
+     <div class="table-responsive table-hover" id="tablaConsulta">
+        <table class="table">
+          <thead class="text-muted">
+              <th class="text-center">Número de identificación</th>
+              <th class="text-center">Tipo</th>
+              <th class="text-center">Nombres</th>
+              <th class="text-center">Apellidos</th>
+              <th class="text-center">Fecha y hora de cita</th>
+              <th class="text-center">Estado de ingreso</th>
+          </thead>
+          <tbody>
+            <tr>
+                <td></td>
+            </tr>
+          </tbody>
+        </table></
+     </div>
+  </div>
+</div>
 
 <?php include('../plantillas/footer.php'); ?>
